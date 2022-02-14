4875aeea1575ec3433da0298827714ff2c1131fe
jurab <brazdil94@gmail.com>
2022-02-14 15:14:15 +0000

    Update client.py

--- client.py
+++ client.py
@@ -7,13 +7,15 @@ import json
 openai.api_key = os.getenv("OPENAI_KEY")
 s3 = boto3.resource('s3')
 
-start_prompt = """The following is a transcript of a cooking lesson. The chef comments on the pupil's cooking itself and the narrator explains what the expert chef means.
+START_PROMPT = """The following is a transcript of a cooking lesson. The chef talks to the pupil. The narrator summarizes for the viewers what the chef says.
 
-Chef: The pupil just finished cooking, so let's see how did it go.
+Chef: Let's start.
 Narrator: The chef is about to start the lesson."""
 
+MID_PROMPT = "The lesson is over. Let's talk to the pupil now to find out what they learned."
 
-questions = (
+
+QUESTIONS = (
     "What dish have you made today?",
     "How did the chef describe their vision for the dish?",
     "Did you have any moments of doubt?",
@@ -24,16 +26,6 @@ questions = (
     "What's the reasoning, or science behind it?",
     "When do you have to react?",
     "What happens when it goes wrong?",
-)
-
-_questions = (
-    "What dish have you made today?",
-    "How did the chef describe their vision for the dish?",
-    "Did you mention any moments of doubt to the chef?",
-    "What part of the dish did the chef focus on?",
-    "What would the chef like you to improve on that?",
-    "What's the best way to do that?",
-    "Did the chef not focus on anything else?"
 )
 
 
@@ -48,16 +40,16 @@ class BotoError(Exception):
     pass
 
 
-class Narrator:
+class Narrator(Models):
 
     model = Models.DAVINCI
     temperature = 0.5
-    max_tokens=150
+    max_tokens=50
     top_p=1
     frequency_penalty=0
-    presence_penalty=0.6
+    presence_penalty=0
 
-    def _predict_next(self, prompt:str=start_prompt, stop:list=["Chef:", "Narrator:"]) -> str:
+    def _predict_next(self, prompt:str=START_PROMPT, stop:list=["Chef:", "Narrator:"]) -> str:
         response = openai.Completion.create(
           engine=self.model,
           prompt=prompt,
@@ -86,25 +78,26 @@ class Narrator:
 
     def run(self, filename:str, model=Models.ADA) -> str:
         sentences = [item.strip() + '.' for item in open(f"transcripts/{filename}", 'r').read().strip().split('.') if item]
+        sentence_pairs = [' '.join(pair) for pair in zip(sentences[::2], sentences[1::2])]
 
         # CHEF FEEDBACK
-        prompt = self._converse(start_prompt, 'Chef', 'Narrator', sentences)
+        prompt = self._converse(START_PROMPT, 'Chef', 'Narrator', sentence_pairs)
 
         # INQUIRIES
-        prompt += "\nA\n\n"
-        prompt = self._converse(prompt, 'Narrator', 'Pupil', questions)
+        # prompt += f"\n\n{MID_PROMPT}\n"
+        # prompt = self._converse(prompt, 'Narrator', 'Pupil', QUESTIONS)
 
         return prompt
-
 
 
 class Client:
     bucket = "moment-assets-prod"
     prefix = "uploads/feedback/feedback_audio"
+    bucket_url = "https://s3.console.aws.amazon.com/s3/object/moment-assets-prod"
 
-    def __init__(self, feedback_id:str):
+    def __init__(self, feedback_id:str=None):
         self.feedback_id = feedback_id
-        self.s3_url = f"https://s3.console.aws.amazon.com/s3/object/moment-assets-prod?region=eu-west-2&prefix={self.prefix}/{self.feedback_id}/mp3_blob.mp3"
+        self.s3_url = f"{self.bucket_url}?region=eu-west-2&prefix={self.prefix}/{self.feedback_id}/mp3_blob.mp3"
 
     def narrate(self, filename:str) -> str:
         return Narrator().run(filename, model=Models.DAVINCI)
@@ -114,5 +107,10 @@ class Client:
         return bool(list_response.get('Contents', None))
 
     def transcribe(self, feedback_id: str):
-        if not _feedback_exists(feedback_id):
+        if not self._feedback_exists(feedback_id):
             raise BotoError(f"Feedback not found at {self.bucket}/{self.prefix}/{feedback_id}")
+
+
+client = Client()
+out = client.narrate("aki_paella.txt")
+print(out)
